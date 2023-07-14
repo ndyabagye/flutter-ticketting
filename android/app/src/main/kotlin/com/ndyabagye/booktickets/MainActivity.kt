c30cf986fee0c62228c86fdcdaea85895898e26d
Ndyabagye Henry <ndyabagyehenrytusi@gmail.com>
2023-07-14 19:07:24 +0300

    First commit

---
+++ android/app/src/main/kotlin/com/ndyabagye/booktickets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ndyabagye.booktickets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/app_info.dart
@@ -0,0 +1,83 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'one.png',
+    'place': 'Open Space',
+    'destination': 'London',
+    'price': 25
+  },
+  {
+    'image': 'two.png',
+    'place': 'Global Will',
+    'destination': 'Madrid',
+    'price': 40
+  },
+  {
+    'image': 'three.png',
+    'place': 'Tallest Building',
+    'destination': 'Dubai',
+    'price': 68
+  },
+  {
+    'image': 'four.png',
+    'place': 'Ol Country Lane',
+    'destination': 'England',
+    'price': 87
+  },
+  {
+    'image': 'five.png',
+    'place': 'Versailles',
+    'destination': 'Spain',
+    'price': 90
+  },
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {'code': "NYC", 'name': "New-York"},
+    'to': {'code': "LDN", 'name': "London"},
+    'flying_time': '8H 30M',
+    'date': "1 MAY",
+    'departure_time': "08:00 AM",
+    "number": 23
+  },
+  {
+    'from': {'code': "DK", 'name': "Dhaka"},
+    'to': {'code': "SH", 'name': "Shanghai"},
+    'flying_time': '4H 20M',
+    'date': "10 MAY",
+    'departure_time': "09:00 AM",
+    "number": 45
+  },
+  {
+    'from': {'code': "PAR", 'name': "Paris"},
+    'to': {'code': "ROM", 'name': "Rome"},
+    'flying_time': '2H 30M',
+    'date': "15 JUN",
+    'departure_time': "10:30 AM",
+    "number": 12
+  },
+  {
+    'from': {'code': "LAX", 'name': "Los Angeles"},
+    'to': {'code': "SYD", 'name': "Sydney"},
+    'flying_time': '14H 45M',
+    'date': "10 AUG",
+    'departure_time': "09:15 PM",
+    "number": 7
+  },
+  {
+    'from': {'code': "LAX", 'name': "Los Angeles"},
+    'to': {'code': "SYD", 'name': "Sydney"},
+    'flying_time': '14H 45M',
+    'date': "10 AUG",
+    'departure_time': "09:15 PM",
+    "number": 7
+  },
+  {
+    'from': {'code': "MEL", 'name': "Melbourne"},
+    'to': {'code': "HKG", 'name': "Hong Kong"},
+    'flying_time': '9H 20M',
+    'date': "5 NOV",
+    'departure_time': "06:30 AM",
+    "number": 9
+  }
+];
