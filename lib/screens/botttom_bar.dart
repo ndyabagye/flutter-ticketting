e06101eecd0dc7b28434716c77764e8dc3623ba5
Ndyabagye Henry <ndyabagyehenrytusi@gmail.com>
2023-07-15 11:42:18 +0300

    Created new launcher icons

--- lib/screens/botttom_bar.dart
+++ lib/screens/botttom_bar.dart
@@ -1,4 +1,5 @@
 import 'package:booktickets/screens/home_screen.dart';
+import 'package:booktickets/screens/profile_screen.dart';
 import 'package:booktickets/screens/search_screen.dart';
 import 'package:booktickets/screens/tickets_screen.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
@@ -18,7 +19,7 @@ class _BottomBarState extends State<BottomBar> {
     const HomeScreen(),
     const SearchScreen(),
     const TicketsScreen(),
-    const Text("Profile"),
+    const ProfileScreen(),
   ];
 
   void _onItemTapped(int index) {
